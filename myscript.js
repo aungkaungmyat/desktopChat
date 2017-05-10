b1bb74a5d56a944d440be73c31f3a59b29d8100c
aungkaungmyat <aungkaungmyat.olaf@gmail.com>
2017-05-10 18:54:58 +0630

    GG errors

--- myscript.js
+++ myscript.js
@@ -1,8 +1,24 @@
+var fs = require('fs');
+var path = require('path');
+
+var filePath = path.join(__dirname, 'userinfo.txt');
+
 var button = document.getElementById("theButton");
 var name1;
 var password1;
 button.onclick = function(){
   name1 = document.getElementById('name').value;
   password1 = document.getElementById('password').value;
-  console.log("hi");
+  console.log("name is " + name1 + " and password is " + password1);
 }
+
+function auth(tname, tpass){
+  if(password1.localeCompare(tpass) == 0){
+    return true;
+  }
+  return false;
+}
+
+fs.readFile(filePath, 'utf8', function(err, contents) {
+    console.log(contents);
+});
