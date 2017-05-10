2ccf4689583baee78d882a7378c02a5d7d27b016
aungkaungmyat <aungkaungmyat.olaf@gmail.com>
2017-05-10 15:28:21 +0630

    first commit

---
+++ myscript.js
@@ -0,0 +1,8 @@
+var button = document.getElementById("theButton");
+var name1;
+var password1;
+button.onclick = function(){
+  name1 = document.getElementById('name').value;
+  password1 = document.getElementById('password').value;
+  console.log("hi");
+}
